ce31a64667011a1692c154b820923c0aacaec19c
Matheus Castro <82881855+239Matheus@users.noreply.github.com>
2025-03-31 18:21:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,12 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-# Defina um token personalizado que você vai usar no painel do Meta
 VERIFY_TOKEN = "matheus123"
 
 @app.route("/webhook", methods=["GET", "POST"])
 def webhook():
     if request.method == "GET":
-        token_sent = request.args.get("hub.verify_token")
-        if token_sent == VERIFY_TOKEN:
+        if request.args.get("hub.verify_token") == VERIFY_TOKEN:
             return request.args.get("hub.challenge")
         return "Token de verificação inválido.", 403
 
@@ -17,6 +15,3 @@ def webhook():
         data = request.json
         print("Mensagem recebida:", data)
         return "Mensagem recebida", 200
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
